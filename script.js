0266406c79c4def9b53ded99adbfd5117e5ca419
Nikola Lazovic <nikolalazovic@tissaro.com>
2020-02-04 23:43:54 -0500

    Commit 2

--- script.js
+++ script.js
@@ -3,6 +3,35 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var lowerCaseCharacters = false;
+  var upperCaseCharacters = false;
+  var numericCharacters = false;
+  var specialCharacters = false; 
+  var passwordLength = 0;
+  while ((passwordLength < 8) || (passwordLength > 128)) {
+    passwordLength = prompt("Password Length (8-128 Char)");
+    console.log(passwordLength);   
+  }
+
+  var yn = prompt("Would you like to include Lower case characters (Y/N)");
+  if (yn.toUpperCase() === "Y") {
+    lowerCaseCharacters = true;
+  }
+  var yn = prompt("Would you like to include Upper case characters (Y/N)");
+  if (yn.toUpperCase() === "Y") {
+    upperCaseCharacters = true;
+  }
+  var yn = prompt("Would you like to include Numeric characters (Y/N)");
+  if (yn.toUpperCase() === "Y") {
+    numericCharacters = true;
+  }
+  var yn = prompt("Would you like to include Special characters (Y/N)");
+  if (yn.toUpperCase() === "Y") {
+    specialCharacters = true;
+  }
+
+  console.log("All Questions Asked")
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
