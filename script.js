8c9dd03662a76b5d570bcb624d8d9d1d90fa3953
Nikola Lazovic <nikolalazovic@tissaro.com>
2020-02-06 09:51:50 -0500

    Script Finished

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var useLowerCaseCharacters = false;
+var useUpperCaseCharacters = false;
+var useNumericCharacters = false;
+var useSpecialCharacters = false; 
+var passwordLength = 0;
+
+var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
+var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericCharacters = "0123456789";
+var specialCharacters = "<>?,.!@#$%^&*()_+";
+
 // Write password to the #password input
 function writePassword() {
-  var lowerCaseCharacters = false;
-  var upperCaseCharacters = false;
-  var numericCharacters = false;
-  var specialCharacters = false; 
-  var passwordLength = 0;
+
   while ((passwordLength < 8) || (passwordLength > 128)) {
     passwordLength = prompt("Password Length (8-128 Char)");
     console.log(passwordLength);   
@@ -15,19 +22,19 @@ function writePassword() {
 
   var yn = prompt("Would you like to include Lower case characters (Y/N)");
   if (yn.toUpperCase() === "Y") {
-    lowerCaseCharacters = true;
+    useLowerCaseCharacters = true;
   }
   var yn = prompt("Would you like to include Upper case characters (Y/N)");
   if (yn.toUpperCase() === "Y") {
-    upperCaseCharacters = true;
+    useUpperCaseCharacters = true;
   }
   var yn = prompt("Would you like to include Numeric characters (Y/N)");
   if (yn.toUpperCase() === "Y") {
-    numericCharacters = true;
+    useNumericCharacters = true;
   }
   var yn = prompt("Would you like to include Special characters (Y/N)");
   if (yn.toUpperCase() === "Y") {
-    specialCharacters = true;
+    useSpecialCharacters = true;
   }
 
   console.log("All Questions Asked")
@@ -36,6 +43,32 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+}
+
+function generatePassword() {
+  var password = "";
+  var allowedCharacters = "";
+  if (useLowerCaseCharacters == true) {
+    allowedCharacters = allowedCharacters + lowerCaseCharacters;
+  }
+  if (useUpperCaseCharacters == true) {
+    allowedCharacters = allowedCharacters + upperCaseCharacters;
+  }
+  if (useNumericCharacters == true) {
+    allowedCharacters = allowedCharacters + numericCharacters;
+  }
+  if (useSpecialCharacters == true) {
+    allowedCharacters = allowedCharacters + specialCharacters;
+  }
+
+  for (var i = 0; i < passwordLength ; i++) {
+    var randomIdx = Math.random()*allowedCharacters.length;
+    randomIdx = Math.round(randomIdx);
+    password = password + allowedCharacters[randomIdx];
+  }
+
+  return password;
+
 
 }
 
